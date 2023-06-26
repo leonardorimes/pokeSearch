a3b931a6e679fac4b93fb8e5dda7e9235a4c8fe8
Leonardo <rimesleo@gmail.com>
2023-06-26 10:37:33 -0300

    adicionando imagens dos pokemons via api

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,24 +3,27 @@ import { useState, useEffect } from "react";
 
 
 const pokemonURL = import.meta.env.VITE_API_Original151
+const pokemonImage = import.meta.env.VITE_GET_IMAGE
+const pokemonSearch = import.meta.env.VITE_SEARCH 
+
 
 
 const Home = () => {
     const [originalPokemon, setOriginalPokemon] = useState([]);
-    const [originalPokemonImage, setOriginalPokemonImage] = useState([]);
-
+    
 
     const getOriginalPokemon = async (url) => {
         const res = await fetch(url)
         const data = await res.json()
-
+        console.log(data)
         setOriginalPokemon(data.results)
         
     }
 
-    // const getPokemonImage (url) => {
+    const PokemonImageURL = (index) => {
+        return `${pokemonImage}${index+1}.png` 
+    }   
 
-    // }
 
     useEffect(() => {
         getOriginalPokemon(pokemonURL)
@@ -30,18 +33,24 @@ const Home = () => {
   return (
     <div>
 
-        {console.log(originalPokemon)}
+        
          <h1> 
           Original Pokemon
         </h1>
         <div className="pokemon-container">
             {originalPokemon.length === 0 && <p> Carregando </p>}
-            {originalPokemon.map((pokemon) => (
-                <>
+            {originalPokemon.map((pokemon, index) => (
+                <div key={index}>
                     <p>
-                        {pokemon.name}
+                        
+                       { <img src={PokemonImageURL(index)} />  }  
+                    
+                       {pokemon.name}
+                    
+                       
+                        
                     </p>
-                </>
+                </div>
             ))}
         </div>
 
